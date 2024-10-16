ca986bb0bcb6cec868fda8bf20d21ac7c5965d52
luis perylo <lfpb@cesar.school>
2024-10-16 09:38:41 -0300

    models nota , ranking, turma, aluno corrigidos

--- meuprojeto/aluno/models.py
+++ meuprojeto/aluno/models.py
@@ -1,12 +1,11 @@
-from django.db import models
 from django.contrib.auth.models import User
-
-# Create your models here.
+from django.db import models
 
 class Aluno(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     matricula = models.CharField(max_length=20, unique=True)
     data_nascimento = models.DateField(null=True, blank=True)
+    turma = models.ForeignKey('turma.Turma', on_delete=models.SET_NULL, null=True, blank=True, related_name='alunos_da_turma')
 
     def __str__(self):
         return self.user.username
--- meuprojeto/simulado/models.py
+++ meuprojeto/simulado/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Simulado(models.Model):
+    nome = models.CharField(max_length=100)
+    data_realizacao = models.DateField()
+    alunos_participantes = models.ManyToManyField('aluno.Aluno', related_name='simulados')
+
+    def __str__(self):
+        return self.nome
