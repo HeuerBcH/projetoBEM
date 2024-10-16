28c8f075fa349e940b6641bc6b16dc685d89fab6
HeuerBcH <bernardoheuer2005@gmail.com>
2024-10-16 12:26:46 -0300

    Mudanças no models e front end das turmas

--- meuprojeto/turma/models.py
+++ meuprojeto/turma/models.py
@@ -1,12 +1,19 @@
 from django.db import models
-
+from django.utils import timezone
 
 # Create your models here.
 
+
 class Turma(models.Model):
-    nome = models.CharField(max_length=100)
-    horario = models.FloatField()
-    dias = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100, verbose_name="Nome da Turma")
+    horario_inicio = models.TimeField(verbose_name="Horário de Início", default='06:00')  # Adiciona um valor padrão
+    horario_fim = models.TimeField(verbose_name="Horário de Fim", default='07:00')  # Adiciona um valor padrão
+    dias = models.CharField(max_length=100, verbose_name="Dias da Semana")  
+    professor = models.CharField(max_length=100, verbose_name="Professor Responsável", blank=True, null=True)
 
     def __str__(self):
-        return self.nome
+        return f"{self.nome} ({self.horario_inicio} - {self.horario_fim})"
+
+    class Meta:
+        verbose_name = "Turma"
+        verbose_name_plural = "Turmas"
