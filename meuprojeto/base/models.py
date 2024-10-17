ce58f9f1fc5b0e1d62a7f9e9f589af48cb138610
Rodrigo <you@example.com>
2024-10-17 20:50:14 -0300

    ok

--- meuprojeto/base/models.py
+++ meuprojeto/base/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 
 class Usuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='logos/')
 
     def __str__(self):
         return self.user.username
