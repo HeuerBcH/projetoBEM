c02489ff3055fb7de35d394f7946e819402cab5e
HeuerBcH <bernardoheuer2005@gmail.com>
2024-10-17 22:26:38 -0300

    Alterações de front end

--- meuprojeto/base/models.py
+++ meuprojeto/base/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 
 class Usuario(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='logos/')
 
     def __str__(self):
         return self.user.username
