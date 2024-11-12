982700cbae7ac7da9cd6723611f6d60b79aa3d99
HeuerBcH <bernardoheuer2005@gmail.com>
2024-11-11 22:16:57 -0300

    Exclusao de simulados, Pagina de Info de simulados

--- meuprojeto/simulado/urls.py
+++ meuprojeto/simulado/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('criar_simulado_pagina/', views.criar_simulado_pagina, name='criar_simulado_pagina'),
     path('criar_simulado/', views.criar_simulado, name='criar_simulado'),
     path('listar_simulados/', views.listar_simulados, name='listar_simulados'),
+    path('excluir_simulado/<int:simulado_id>/', views.excluir_simulado, name='excluir_simulado'),
+    path('info_simulado/<int:simulado_id>/', views.info_simulado, name='info_simulado'),
+    path('editar_simulado/<int:simulado_id>/', views.editar_simulado, name='editar_simulado'),
 
 ]
